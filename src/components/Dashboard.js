cf97c98a29922944e6d26947fd90dabe95ca8c38
AJ Genung <DragonRydrz@me.com>
2018-06-16 01:30:42 -0400

    25 levels and remembers the highest level completed

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,10 +3,19 @@ import InfoBox from './InfoBox';
 
 class Dashboard extends Component {
   render() {
-    const { dashboardStyles, boxStyles } = styles;
+    const { dashboardStyles, boxStyles, changeLevelStyles } = styles;
+    const left = '<-';
+    const right = '->';
     return (
       <div style={dashboardStyles}>
+        <button style={changeLevelStyles} onClick={this.props.prevLevel}>
+          {left}
+        </button>
         <InfoBox title="Level" data={this.props.level} />
+        <button style={changeLevelStyles} onClick={this.props.nextLevel}>
+          {right}
+        </button>
+
         <InfoBox title="Moves" data={this.props.moveCount} />
         <button style={boxStyles} onClick={this.props.reset}>
           RESET
@@ -19,6 +28,7 @@ class Dashboard extends Component {
 const styles = {
   dashboardStyles: {
     display: 'flex',
+    justifyContent: 'space-around',
     width: 299,
     margin: '20px auto',
     border: '1px solid black',
@@ -32,6 +42,18 @@ const styles = {
     margin: 5,
     border: '1px solid black',
     textAlign: 'center',
+    backgroundColor: 'gray',
+  },
+  changeLevelStyles: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: 30,
+    height: 20,
+    alignSelf: 'center',
+    margin: 5,
+    border: '1px solid black',
+    textAlign: 'center',
+    backgroundColor: 'gray',
   },
 };
 
--- src/components/LightsOut.js
+++ src/components/LightsOut.js
@@ -14,12 +14,35 @@ class LightsOut extends React.Component {
       4: [0, 6, 12, 18, 24],
       5: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
       6: [10, 12, 14],
+      7: [0, 2, 4, 5, 7, 915, 17, 19, 20, 22, 24],
+      8: [1, 3, 5, 6, 8, 9, 10, 11, 13, 14, 15, 16, 18, 19, 21, 23],
+      9: [5, 6, 8, 9, 15, 19, 20, 21, 23, 24],
+      10: [0, 1, 2, 3, 5, 6, 7, 9, 10, 11, 12, 14, 18, 19, 20, 21, 23, 24],
+      11: [10, 12, 14, 15, 17, 19, 21, 22, 23],
+      12: [0, 1, 2, 3, 5, 9, 10, 14, 15, 19, 20, 21, 22, 23],
+      13: [7, 11, 13, 15, 17, 19, 21, 23],
+      14: [1, 3, 5, 6, 7, 8, 9, 11, 12, 13, 16, 18, 19, 20, 21, 22],
+      15: [1, 2, 3, 6, 7, 8, 11, 12, 13],
+      16: [0, 2, 4, 5, 7, 9, 10, 12, 14, 15, 17, 19, 21, 22, 23],
+      17: [0, 1, 2, 3, 4, 6, 8, 10, 11, 13, 14, 16, 17, 18, 21, 23],
+      18: [3, 7, 9, 11, 13, 15, 17, 21],
+      19: [11, 16, 21],
+      20: [12, 16, 17, 18, 20, 21, 22, 23, 24],
+      21: [2, 6, 8, 10, 12, 14, 16, 18, 22],
+      22: [0, 2, 4, 10, 12, 14, 20, 22, 24],
+      23: [1, 2, 3, 4, 6, 11, 12, 13, 16, 21],
+      24: [10, 14, 15, 16, 17, 18, 19, 21, 24],
+      25: [0, 5, 6, 10, 11, 12, 15, 16, 17, 18, 21, 22, 23, 24],
     },
   };
   componentDidMount() {
-    const maxLevel = localStorage.getItem('maxLevel');
-    if (maxLevel) this.setState({ currentLevel: maxLevel });
-    this.setToLevel();
+    const maxLevel = Number(localStorage.getItem('maxLevel'));
+    if (maxLevel) {
+      this.setState({ currentLevel: maxLevel + 1 });
+      this.setToLevel(maxLevel + 1);
+    } else {
+      this.setToLevel(1);
+    }
   }
   render() {
     const appStyles = {
@@ -29,16 +52,33 @@ class LightsOut extends React.Component {
     };
     return (
       <div style={appStyles}>
-        {this.state.levelCompleted ? this.levelCompleted() : null}
         <Grid boxes={this.state.boxes} toggle={this.toggle} />
         <Dashboard
           level={this.state.currentLevel}
           moveCount={this.state.moveCount}
           reset={this.setToLevel}
+          nextLevel={this.nextLevel}
+          prevLevel={this.prevLevel}
         />
       </div>
     );
   }
+
+  nextLevel = () => {
+    console.log(Object.keys(this.state.levels).length);
+    if (this.state.currentLevel < Object.keys(this.state.levels).length) {
+      this.setState({ currentLevel: this.state.currentLevel + 1 });
+      this.setToLevel(this.state.currentLevel + 1);
+    }
+  };
+
+  prevLevel = () => {
+    if (!(this.state.currentLevel <= 1)) {
+      this.setState({ currentLevel: this.state.currentLevel - 1 });
+      this.setToLevel(this.state.currentLevel - 1);
+    }
+  };
+
   toggle = index => {
     const boxes = [...this.state.boxes];
     const top = index - 5;
@@ -62,12 +102,12 @@ class LightsOut extends React.Component {
         : this.state.currentLevel,
     });
     console.log(this.state.moveCount);
-    // if (!boxes.includes(true)) this.levelCompleted();
+    if (!boxes.includes(true)) this.levelCompleted();
   };
 
-  setToLevel = () => {
+  setToLevel = level => {
     // const boxes = [...this.state.boxes];
-    const level = this.state.currentLevel;
+    // const level = this.state.currentLevel;
     const boxes = this.state.boxes.map((box, index) => {
       return this.state.levels[level].includes(index);
     });
@@ -76,8 +116,8 @@ class LightsOut extends React.Component {
 
   levelCompleted = () => {
     console.log('completed');
-    // localStorage.setItem('maxLevel', this.state.currentLevel);
-    this.setToLevel();
+    localStorage.setItem('maxLevel', this.state.currentLevel);
+    this.setToLevel(this.state.currentLevel + 1);
   };
 }
 
